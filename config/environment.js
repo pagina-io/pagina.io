c1b9fbf6eabccf0584deb34ed584c74ecd542297
Adriaan van Rossum <git@adriaan.io>
2015-06-19 12:57:42 +0200

    Fix syntax error

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
     HOSTS: {
       // We will put our host urls here
     }
