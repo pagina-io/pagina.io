86c10ea08db2e01df8cc6675dfc784a7ace88960
Adriaan van Rossum <git@adriaan.io>
2015-06-26 18:00:32 +0200

    Parse yaml

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/showdown/dist/showdown.min.js');
+app.import('bower_components/js-yaml/dist/js-yaml.min.js');
 
 module.exports = app.toTree();
--- app/user/repo/file/controller.js
+++ app/user/repo/file/controller.js
@@ -1,3 +1,5 @@
+/* globals showdown, jsyaml */
+
 import Ember from 'ember';
 
 export
@@ -6,15 +8,30 @@ default Ember.Controller.extend({
   editing: false,
 
   html: function() {
-
     var converter = new showdown.Converter();
     return converter.makeHtml(this.get('model.content'));
-
   }.property('model.content'),
+
+  yaml: function() {
+    var header = this.get('model.content').split('---')[1].trim();
+    return jsyaml.load(header);
+  }.property('model.content'),
+
+  layout: function() {
+    return this.get('yaml').layout;
+  }.property('yaml'),
+
+  title: function() {
+    return this.get('yaml').title || 'Title is not found';
+  }.property('yaml'),
 
   actions: {
 
     edit: function() {
+      this.toggleProperty('editing');
+    },
+
+    cancel: function() {
       this.toggleProperty('editing');
     },
 
