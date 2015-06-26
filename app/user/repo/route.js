2e7768b473fb2cffc67e90d0b63930b128cc3c44
Adriaan van Rossum <git@adriaan.io>
2015-06-26 14:51:49 +0200

    Do a post request to repo when accessing repo route

--- app/user/repo/route.js
+++ app/user/repo/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export
+default Ember.Route.extend({
+
+  model: function(params) {
+    return this.store.createRecord('repo', {
+      name: params.repo,
+      userId: 1
+    }).save().then(function(repo) {
+      return repo;
+    });
+  }
+
 });
