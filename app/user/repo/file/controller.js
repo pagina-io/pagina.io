8c820891dfa21afa29dccf95ff540502b0ed8c5d
Adriaan van Rossum <git@adriaan.io>
2015-06-26 17:43:33 +0200

    Use default texture for editing files

--- app/user/repo/file/controller.js
+++ app/user/repo/file/controller.js
@@ -5,6 +5,13 @@ default Ember.Controller.extend({
 
   editing: false,
 
+  html: function() {
+
+    var converter = new showdown.Converter();
+    return converter.makeHtml(this.get('model.content'));
+
+  }.property('model.content'),
+
   actions: {
 
     edit: function() {
