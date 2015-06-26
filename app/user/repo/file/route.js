7a67e1f0ea6fc893e9dd51ab2671648c997593a8
Adriaan van Rossum <git@adriaan.io>
2015-06-26 15:45:04 +0200

    Start with file route

--- app/user/repo/file/route.js
+++ app/user/repo/file/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export
+default Ember.Route.extend({
+
+  model: function(params) {
+    console.log(params)
+  }
+
 });
--- app/user/repo/index/route.js
+++ app/user/repo/index/route.js
@@ -3,12 +3,13 @@ import Ember from 'ember';
 export
 default Ember.Route.extend({
 
-  model: function(params) {
+  model: function(params, transition) {
 
     var user = this.modelFor('user');
+    var repo = transition.params['user.repo'].repo;
 
     return this.store.createRecord('repo', {
-      name: params.repo,
+      name: repo,
       userId: user.user_id
     }).save().then(function(repo) {
       return repo;
