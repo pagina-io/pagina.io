efa1efe5b80bd444bc906d7036d84ed659e4bcbb
Adriaan van Rossum <git@adriaan.io>
2015-07-24 16:42:02 +0200

    Remove unneeded code from environment.js

--- config/environment.js
+++ config/environment.js
@@ -22,33 +22,11 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.HOSTS.api = 'http://localhost:9292';
   }
 
   if (environment === 'acceptance') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.HOSTS.api = 'http://api.jikkyll.fiiv.io';
-  }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
@@ -67,3 +45,4 @@ module.exports = function(environment) {
 
   return ENV;
 };
+
