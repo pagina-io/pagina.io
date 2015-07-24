cd88c471c9cea78976bb3dd3e49f338d8dc295ee
Adriaan van Rossum <git@adriaan.io>
2015-07-24 14:42:53 +0200

    Get file content via our own api

--- app/user/repo/file/route.js
+++ app/user/repo/file/route.js
@@ -1,25 +1,19 @@
 import Ember from 'ember';
-import ENV from 'jikkyll/config/environment';
 
 export
 default Ember.Route.extend({
 
   model: function(params, transition) {
-    var env = ENV.default || ENV;
     var repo = transition.params['user.repo'].repo;
-    var token = JSON.parse(window.localStorage.auth).access_token;
-    var url = env.HOSTS.api + '/git/repos/' + transition.params['user'].username + '/' +
-      transition.params['user.repo'].repo + '/contents/' + params.filePath + '?access_token=' + token;
-
-    return Ember.$.ajax(url).then(function(data) {
-      var result = data.result;
-      this.store.pushPayload('file', {
-        files: [{
-          id: result.sha,
-          content: window.atob(result.content)
-        }]
-      });
-      return this.store.find('file', result.sha);
+    return this.store.find('repofile', {
+      filename: params.filePath,
+      repo_name: repo
+    }).then(function(files) {
+      if (Ember.isEmpty(files.get('firstObject.id'))) {
+        return;
+      }
+      this.store.unloadRecord(files.get('firstObject'));
+      return this.store.find('repofile', files.get('firstObject.id'));
     }.bind(this));
   }
 
