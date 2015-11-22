8c2d83ec9c43bb6b5ba60709dbffba0d4016fe9c
Adriaan van Rossum <git@adriaan.io>
2015-11-22 14:02:41 +0100

    Update to newest ember 1.x version

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
+const Router = Ember.Router.extend({
   location: config.locationType
 });
 
@@ -22,4 +22,3 @@ Router.map(function() {
 
 export
 default Router;
-
